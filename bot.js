ec47a16a97bf18f1ebf1a9e5b662cb324546bd2a
Benjamin Mo <b.mo96@hotmail.com>
2017-06-06 21:35:53 +1000

    all .json are ignored

--- bot.js
+++ bot.js
@@ -1,3 +1,9 @@
+/*
+ * AFK-Bot
+ * A bot for personal use on ONE server
+ * Developed by Benjamin Mo
+ */
+
 const Discord = require('discord.js');
 const config = require('./config.json');
 const fs = require('fs');
@@ -22,11 +28,11 @@ client.on('message', (message) => {
     message.channel.send('bar!');
   } 
 
-  // Owner-only commands
+  // Owner-only commands below
+  if(message.author.id !== config.ownerID) return;
+
   if (message.content.startsWith(config.prefix + 'eval')) {
-    if(message.author.id !== config.ownerID) return;
     // Eval command
-    
     try {
       const code = args.join(' ');
       let evaled = eval(code);
@@ -38,28 +44,102 @@ client.on('message', (message) => {
     } catch (err) {
       message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
     }
-  } else if(message.content.startsWith(config.prefix + 'prefix')) {
-    if(message.author.id !== config.ownerID) return;
+  } else if (message.content.startsWith(config.prefix + 'prefix')) {
     // Change prefix command
-
-    // Gets the prefix from the command (eg. "!prefix +" it will take the "+" from it)
     let newPrefix = message.content.split(' ').slice(1, 2)[0];
-    // change the configuration in memory
     config.prefix = newPrefix;
-    // Now we have to save the file.
     fs.writeFile('./config.json', JSON.stringify(config, null, 2), (err) => { if(err) console.error(err) });
+    
     message.channel.send("Prefix changed to '" + config.prefix + "'.");
+  } else if (message.content.startsWith(config.prefix + 'log_channel') || 
+             message.content.startsWith(config.prefix + 'lc')) {
+    // Change log channel
+    config.logChannelID = message.channel.id;
+    fs.writeFile('./config.json', JSON.stringify(config, null, 2), (err) => { if(err) console.error(err) });
+    message.channel.send("Log channel changed to **" + message.channel.name + "**");
+  } else if (message.content.startsWith(config.prefix + 'main_channel') || 
+             message.content.startsWith(config.prefix + 'mc')) {
+    // Change main channel (#general in most servers)
+    config.mainChannelID = message.channel.id;
+    fs.writeFile('./config.json', JSON.stringify(config, null, 2), (err) => { if(err) console.error(err) });
+    message.channel.send("Main channel changed to **" + message.channel.name + "**");
   }
 });
 
+
+// Sends a message in the log channel (config.logChannelID) 
+// when a user joins or leaves voice channel
 client.on('voiceStateUpdate', (oldMember, newMember) => {
   if (oldMember.voiceChannel == null) {
-    console.log(newMember.user.username + " joined voice channel \"" + newMember.voiceChannel.name + "\".");
+    client.channels.get(config.logChannelID).send(
+      newMember.user.username + " joined voice channel **" + newMember.voiceChannel.name + "**."
+    );
   } else if (newMember.voiceChannel == null) {
-    console.log(newMember.user.username + " left voice channel \"" + oldMember.voiceChannel.name + "\".");
+    client.channels.get(config.logChannelID).send(
+      newMember.user.username + " left voice channel **" + oldMember.voiceChannel.name + "**."
+    );
   } else if (oldMember.voiceChannel != newMember.voiceChannel) {
-    console.log(newMember.user.username + " switched from channel \"" + oldMember.voiceChannel.name + "\" to \"" + newMember.voiceChannel.name + "\".");
+    client.channels.get(config.logChannelID).send(
+      newMember.user.username + " switched from channel **" + oldMember.voiceChannel.name + "** to **" + newMember.voiceChannel.name + "**."
+    );
   }
+});
+
+// Sends a message in the log channel (config.logChannelID)
+// when a user comes online or goes offline
+client.on('presenceUpdate', (oldMember, newMember) => {
+  if (oldMember.presence.status == newMember.presence.status) return;
+
+  if (oldMember.presence.status == 'offline') {
+    // User coming online
+    client.channels.get(config.logChannelID).send(
+      newMember.user.username + " has come online."
+    );
+  } else if (newMember.presence.status == 'offline') {
+    // User going offline
+    client.channels.get(config.logChannelID).send(
+      newMember.user.username + " disconnected."
+    );
+  }
+});
+
+// Sends a message in the log channel (config.logChannelID)
+// when a user's role or nickname is modified
+client.on('guildMemberUpdate', (oldMember, newMember) => {
+  // Nickname changes
+  if (oldMember.nickname == null && newMember.nickname != null) {
+    client.channels.get(config.logChannelID).send(
+      newMember.user.username + "'s nickname was set to **" + newMember.nickname + "**."
+    );
+  } else if (oldMember.nickname != null && newMember.nickname == null) {
+    client.channels.get(config.logChannelID).send(
+      newMember.user.username + "'s nickname **" + oldMember.nickname + "** was removed."
+    );
+  } else if (oldMember.nickname != newMember.nickname) {
+    client.channels.get(config.logChannelID).send(
+      newMember.user.username + "'s nickname was changed from **" + oldMember.nickname + 
+      "** to **" + newMember.nickname  + "**."
+    );
+  }
+
+  // Role changes
+  // To be added later :)
+});
+
+// Send message in main channel 
+// when a new user joins the server
+client.on('guildMemberAdd', (member) => {
+  client.channels.get(config.mainChannelID).send(
+    "**" + member.user.username + "** has joined " + member.guild.name + ". Annyeong!"
+  );
+});
+
+// Send message in main channel 
+// when a user leaves the server
+client.on('guildMemberRemove', (member) => {
+  client.channels.get(config.mainChannelID).send(
+    "**" + member.user.username + "** has left " + member.guild.name + ". Bye!"
+  );
 });
 
 function clean(text) {
